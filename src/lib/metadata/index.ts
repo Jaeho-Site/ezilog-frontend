aa186b74ef2b9438aa5561424256680d11bc4d5d
Jaeho <sjaeho348@gmail.com>
2026-02-04 16:37:31 +0900

    feat : metadata 유틸리티, 생성 파일 export

---
+++ src/lib/metadata/index.ts
@@ -0,0 +1,24 @@
+export { siteConfig, getMetadataBase, getCanonicalUrl, getImageUrl } from './config';
+
+export type {
+  PostMetadataParams,
+  CategoryMetadataParams,
+  SearchMetadataParams,
+  OpenGraphParams,
+  TwitterCardParams,
+  RobotsParams,
+  MetadataResult,
+} from './types';
+
+export {
+  buildBaseMetadata,
+  buildOpenGraph,
+  buildTwitterCard,
+  buildRobots,
+} from './builders';
+
+export { generateHomeMetadata } from './generators/home';
+export { generatePostMetadata, generatePostNotFoundMetadata } from './generators/post';
+export { generateCategoryMetadata, generateCategoryNotFoundMetadata } from './generators/category';
+export { generateSearchMetadata } from './generators/search';
+export { generateLatestMetadata } from './generators/latest';
